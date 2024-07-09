6aa084456e809cec840b3fa0071032940cde881d
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2024-07-09 21:22:45 +0200

    remove unnecessary logging

--- src/Blank.Presentation/Controllers/RandomController.cs
+++ src/Blank.Presentation/Controllers/RandomController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace Blank.Presentation.Controllers;
 
@@ -10,11 +9,6 @@ namespace Blank.Presentation.Controllers;
 [Route("api/random")]
 public sealed class RandomController
 {
-    private readonly ILogger<RandomController> _logger;
-
-    public RandomController(ILogger<RandomController> logger)
-        => _logger = logger;
-
     [HttpGet("number")]
     public ValueTask<int> GetRandomNumber(CancellationToken cancellationToken)
         => ValueTask.FromResult(
